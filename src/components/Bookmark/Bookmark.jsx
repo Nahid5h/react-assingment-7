f63842cd11c4f5d0ae8a955004e180497625dc0c
Nahid5h <nahidhassan02022002@gail.com>
2024-03-17 20:27:30 +0600

    adding the index

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 
-const Bookmark = ({bookmark}) => {
+const Bookmark = ({bookmark,index }) => {
     
     const {recipe_name,preparing_time_minutes,calories ,}=bookmark;
     return (
         <div className='flex justify-between '>
+            <h1>{index+1}</h1>
          
-            <h1 className='text-xl w-16'>  {recipe_name}</h1>
+            <p className=' w-16'>  {recipe_name}</p>
             <p className=''>{preparing_time_minutes} minutes</p>
             <p>{calories}</p>
             <button className='btn bg-[#35f3a3] rounded-full'>Preparing</button>
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -6,13 +6,13 @@ const Bookmarks = ({bookmarks}) => {
         <div className="ml-8 w-1/3 shadow-lg p-10 h-auto space-y-3">
             <h1 className="text-3xl font-bold text-center mb-5">Want to cook:{bookmarks.length} </h1>
             <hr />
-            <div className='text-xl font-semibold flex gap-14 my-5'>
+            <div className='text-xl font-semibold flex gap-14 my-5 ml-7'>
                <h1>Name</h1> 
                <h1>Time</h1> 
                <h1>Calories</h1> 
             </div>
             {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+                bookmarks.map((bookmark,index) => <Bookmark key={bookmark.id} index={index} bookmark={bookmark}></Bookmark>)
             }
         </div>
     );
